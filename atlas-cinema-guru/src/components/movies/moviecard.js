e4c4ba9303646e64d44818df1132f8f7330f9a32
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-07-09 21:44:19 -0500

    movie cards

---
+++ atlas-cinema-guru/src/components/movies/moviecard.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faClock } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+import './movies.css';
+
+const MovieCard = ({ movie }) => {
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [isWatchLater, setIsWatchLater] = useState(false);
+
+    useEffect(() => {
+        // Fetch favorite and watch later status
+        axios.get('/api/titles/favorite/')
+            .then(response => {
+                if (response.data.some(favMovie => favMovie.imdbId === movie.imdbId)) {
+                    setIsFavorite(true);
+                }
+            });
+        axios.get('/api/titles/watchlater/')
+            .then(response => {
+                if (response.data.some(watchLaterMovie => watchLaterMovie.imdbId === movie.imdbId)) {
+                    setIsWatchLater(true);
+                }
+            });
+    }, [movie.imdbId]);
+
+    const handleClick = (type) => {
+        const url = `/api/titles/${type}/${movie.imdbId}`;
+        const isCurrently = type === 'favorite' ? isFavorite : isWatchLater;
+        const setState = type === 'favorite' ? setIsFavorite : setIsWatchLater;
+
+        if (isCurrently) {
+            axios.delete(url).then(() => setState(false));
+        } else {
+            axios.post(url).then(() => setState(true));
+        }
+    };
+
+    return (
+        <li className="movie-card">
+            <div className="icons">
+                <FontAwesomeIcon 
+                    icon={faStar} 
+                    className={`icon ${isFavorite ? 'selected' : ''}`} 
+                    onClick={() => handleClick('favorite')} 
+                />
+                <FontAwesomeIcon 
+                    icon={faClock} 
+                    className={`icon ${isWatchLater ? 'selected' : ''}`} 
+                    onClick={() => handleClick('watchlater')} 
+                />
+            </div>
+            <h2>{movie.title}</h2>
+            <p>{movie.synopsis}</p>
+            <p>Genres: {movie.genres.join(', ')}</p>
+        </li>
+    );
+};
+
+export default MovieCard;
